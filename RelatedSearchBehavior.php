1ebd387f39c0bb5e0c3a3b53f2fd419dc1074c4b
LeoZandvliet <leozandvliet@gmail.com>
2021-06-07 11:53:00 +0200

    Allow related search to be directly used in the $criteria->condition

--- RelatedSearchBehavior.php
+++ RelatedSearchBehavior.php
@@ -327,6 +327,15 @@ class RelatedSearchBehavior extends CActiveRecordBehavior {
             if(in_array("$var",$sort_keys)&&($shortrelation!=='')) {
                 $require_relation=true;
             }
+		
+            // d. If a relation is used in the criteria condition, require it and adjust the condition
+            // Pattern to search for, search if relation is used as whole word only (dont want partials to be replaced)
+            $pattern = '/\b('.preg_quote($var).')\b/';
+            if(preg_match($pattern, $criteria->condition)){
+                $totalReplaced = 0;
+                $require_relation=($shortrelation!=='');
+                $criteria->condition = preg_replace($pattern, $column,$criteria->condition, -1, $totalReplaced);
+            }
 
             // If the relation is required, add it to the search condition.
             if($require_relation) {
